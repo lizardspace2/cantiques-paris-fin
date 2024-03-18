0cf61600ed837be0cd3569936410da28ee81d8ff
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2024-03-18 21:20:35 +0000

    fffffffffffffff

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom"; // Import Navigate for redirection
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; 
 import { ChakraProvider, Heading } from "@chakra-ui/react";
 import HeaderTop from "./components/HeaderTop";
 import Header from "./components/Header";
